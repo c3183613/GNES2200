c1e4671dc1658cffec90d94d8ff05afecf3b1834
Jeremy Law <c3183613@uon.edu.au>
2016-04-27 16:37:40 +1000

    compareTo added and moved to PlanarShape

---
+++ PA2b.java
@@ -0,0 +1,7 @@
+public class PA2b
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
--- SortedList.java
+++ SortedList.java
@@ -16,7 +16,7 @@ public class SortedList<T extends PlanarShape> extends LinkedList<T>
 			while(iterate.hasNext())
 			{
 				next = iterate.next();
-				if(comesBefore(next, smallest)) // if next comes before smallest
+				if(PlanarShape.compareTo(next, smallest)) // if next comes before smallest
 				{
 					smallest = next;
 				}
@@ -36,48 +36,5 @@ public class SortedList<T extends PlanarShape> extends LinkedList<T>
 			}
 		}
 		return sortedList;
-	}
-
-	/*
-	Preconditions:
-	Postconditions: If this Polygon comes before 'o', return true. Else, return false
-					Returns true if: areas are 'equal' and this Polygon's closest Point to
-					the origin is smaller or equal to o's closest Point
-					Returns true if: this Polygon's area is less than o's area
-					Returns false otherwise
-	*/
-	private static boolean comesBefore(PlanarShape shape1, PlanarShape shape2)
-	{
-		// If shape2 and this Polygon are considered equal by area
-		// Check which Polygon has the point closest to the origin
-		if(shape1.equals(shape2))
-		{
-			// if shape2's closest point to origin is bigger than this Polygon's 
-			// closest point to origin, return true
-			if(shape1.originDistance() < shape2.originDistance())
-			{
-				return true;
-			}
-			// if the distance is the same, return true
-			else if(shape1.originDistance() == shape2.originDistance())
-			{
-				return true;
-			}
-			else
-			{
-				// otherwise, return false
-				return false;
-			}
-		}
-		else
-		{
-			// if shape2 has a bigger area, it comes after this
-			// hence this Polygon comes before, return true
-			if(shape2.area() > shape1.area())
-				return true;
-			// othwise, this Polygon's area is bigger, return false
-			else
-				return false;
-		}
 	}
 }
